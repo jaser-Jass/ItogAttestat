34de44c08c417c49bb7ce98219c3691f92674f62
jaser-Jass <yardley1@yandex.ru>
2025-01-29 15:42:08 +0300

    Добавлены стили

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
-    <div>SHOP</div>
+    <div className='wrapper'>
+      <Header />
+      <Footer />
+    </div>
   );
 }
 

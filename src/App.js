d5e564c07c074e0698e1e7e7e2f41c3a9a0bd38d
jaser-Jass <yardley1@yandex.ru>
2025-01-30 13:31:25 +0300

    Добавлен список товаров

--- src/App.js
+++ src/App.js
@@ -1,14 +1,58 @@
+import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './App.css';
+import Items from './components/Items';
 
-function App() {
-  return (
-    <div className='wrapper'>
-      <Header />
-      <Footer />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [
+        {
+        id: 1,
+        title: 'Стул серый',
+        img: 'chair2.jpg',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+        category: 'chairs',
+        price: '49.99'
+        },
+               {
+        id: 2,
+        title: 'Стол',
+        img: 'table3.jpg',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+        category: 'ctables',
+        price: '149.99'
+        },
+                  {
+        id: 3,
+        title: 'Диван',
+        img: 'sofa3.jpg',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+        category: 'sofas',
+        price: '349.00'
+        },
+                         {
+        id: 4,
+        title: 'Шкаф',
+        img: 'wardrobe3.jpg',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+        category: 'sofas',
+        price: '449.00'
+        },
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className='wrapper'>
+        <Header />
+        <Items items={this.state.items} />
+        <Footer />
+      </div>
+    )
+ }
 }
 
 export default App;
